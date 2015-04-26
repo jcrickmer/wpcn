c7fedb4e41f7e48932387f3cbf6201a11fbead3a
Jason Crickmer <jcrickmer@gmail.com>
2015-04-26 09:16:47 -0500

    Baseline for curent cardninja deployment.

---
+++ html/wp-content/themes/responsive/disclaimer.php
@@ -0,0 +1,64 @@
+<?php
+
+// Exit if accessed directly
+if( !defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Disclaimer Page Template
+ *
+Template Name:  Disclaimer Full-width Page (no sidebar)
+ *
+ * @file           disclaimer.php
+ * @package        Responsive
+ * @author         Jason Crickmer
+ * @copyright      2003 - 2014 CyberChimps
+ * @license        license.txt
+ * @version        Release: 1.0
+ * @filesource     wp-content/themes/responsive/disclaimer.php
+ */
+
+get_header(); ?>
+
+<div id="content-full" class="grid col-940">
+
+	<?php if( have_posts() ) : ?>
+
+		<?php while( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'loop-header' ); ?>
+
+			<?php responsive_entry_before(); ?>
+			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<?php responsive_entry_top(); ?>
+
+				<?php get_template_part( 'post-meta-page' ); ?>
+
+				<div class="post-entry">
+					<?php the_content( __( 'Read more &#8250;', 'responsive' ) ); ?>
+					<?php wp_link_pages( array( 'before' => '<div class="pagination">' . __( 'Pages:', 'responsive' ), 'after' => '</div>' ) ); ?>
+				</div>
+				<!-- end of .post-entry -->
+
+				<?php get_template_part( 'post-data' ); ?>
+
+				<?php responsive_entry_bottom(); ?>
+			</div><!-- end of #post-<?php the_ID(); ?> -->
+			<?php responsive_entry_after(); ?>
+
+		<?php
+		endwhile;
+
+		get_template_part( 'loop-nav' );
+
+	else :
+
+		get_template_part( 'loop-no-posts' );
+
+	endif;
+	?>
+
+</div><!-- end of #content-full -->
+
+<?php get_footer(); ?>
